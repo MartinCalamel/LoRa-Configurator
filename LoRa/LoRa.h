c9b0caee71997880c2a937de937f569b602862ae
MartinCalamel <martin.calamel@cpe.fr>
2026-04-22 16:06:58 +0200

    add LoRa.h docstring

--- LoRa/LoRa.h
+++ LoRa/LoRa.h
@@ -21,39 +21,51 @@
  */
 int send_char_from_int(void *msg, size_t size);
 
-/* # config_address
- * ## Fonction
- * Configure address of the device to {address}
- * ## Param
- * {address} : unsigned short => the address you want for the device
- * ## Return
- * {msg} : long int => the msg to send to the device to setup the address
+/**
+ * @brief Configure address of the device to {address}
+ * @param address : unsigned short => the address you want for the device
+ * @return msg : long int => the msg to send to the device to setup the address
+ * @note 0xC0 : commande to set register
+ * @note 0x00 : starting address of the register
+ * @note 0x02 : length of the parameter
  */
 long int config_address(unsigned short address);
 
-/* # config_DataRate
- * ## Fonction
- * Configure Uart DataRate to defaut (9600)
- * ## Return
- * {msg} : int => the msg to send to the device to setup DataRate
+/**
+ * @brief Configure Uart DataRate to defaut (9600)
+ * @param uart  : int => code for the correct baudrate 000 -> 111
+ * @param air   : int => code for the correct data rate 000 -> 111
+ * @return msg  : int => the msg to send to the device to setup DataRate
+ * @note 0xC0 : commande to set register
+ * @note 0x02 : starting address of the register
+ * @note 0x01 : length of the parameter
+ * @note 0x?? : data as follow :
+ * @note    |---Uart rate [7,6,5] => 011 = 9600 (default)
+ * @note    |---Serial parity bit [4,3] => 00 = 8N1 (default)
+ * @note    |---Air DataRate [2,1,0] => 111 : 62.5k (Max)
+ * @note For more details look doc p14-15
  */
 int config_DataRate();
 
-/* # setup_frequency
- * ## Fonction
- * Configure module frequency (850 MHz - 930 MHz)
- * ## Param
- * {freq} : int => frequency you want to setup
- * ## Return
- * {msg} : int => the msg to send to the device to setup frequency
+/**
+ * @brief Configure module frequency
+ * @param freq  : int => frequency you want to setup (850 MHz - 930 MHz)
+ * @return msg  : int => the msg to send to the device to setup frequency
+ * @note 0xC0 : commande to set register
+ * @note 0x04 : starting address of the register
+ * @note 0x01 : length of the parameter
+ * @note data as follow (0-80): freq - 850
  */
 int setup_frequency(int freq);
 
-/* # setup_power
- * ## Fonction
- * Configure power to 13 dBm
- * ## Return
- * {msg} : int => the msg to send to the device to setup power
+/**
+ * @brief Configure power
+ * @param power : int => code for the correct baudrate 00 -> 11
+ * @return msg  : int => the msg to send to the device to setup power
+ * @note 0xC0 : commande to set register
+ * @note 0x03 : starting address of the register
+ * @note 0x01 : length of the parameter
+ * @note power data
  */
 int setup_power();
 
