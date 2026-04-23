094e7c3223a2b7b587e0ef94c644c79b8a9ce947
MartinCalamel <martin.calamel@cpe.fr>
2026-04-23 08:10:21 +0200

    update README so doc

--- docs/search/all_c.js
+++ docs/search/all_c.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['uart_2ec_0',['Uart.c',['../Uart_8c.html',1,'']]],
   ['uart_2eh_1',['Uart.h',['../Uart_8h.html',1,'']]],
-  ['up_5fgpio_2',['up_gpio',['../gpio__utils_8c.html#ac62aae833b894a5204d02812ea705a64',1,'up_gpio(int handle, int gpio_number):&#160;gpio_utils.c'],['../gpio__utils_8h.html#ac62aae833b894a5204d02812ea705a64',1,'up_gpio(int handle, int gpio_number):&#160;gpio_utils.c']]]
+  ['up_5fgpio_2',['up_gpio',['../gpio__utils_8c.html#ac62aae833b894a5204d02812ea705a64',1,'up_gpio(int handle, int gpio_number):&#160;gpio_utils.c'],['../gpio__utils_8h.html#ac62aae833b894a5204d02812ea705a64',1,'up_gpio(int handle, int gpio_number):&#160;gpio_utils.c']]],
+  ['usage_3',['Usage',['../md_README.html#autotoc_md2',1,'']]]
 ];
